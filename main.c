0b576b1b1192c4103a588954cc3688df938903dd
Flavio Carrara De Capua <fde-capu@d3614n.d3614nhost>
2020-04-13 17:40:10 -0300

    putpixel and stuff

--- main.c
+++ main.c
@@ -6,12 +6,16 @@
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/13 05:11:09 by fde-capu          #+#    #+#             */
-/*   Updated: 2020/04/13 15:53:37 by fde-capu         ###   ########.fr       */
+/*   Updated: 2020/04/13 17:30:52 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h> //
-#include <mlx.h>
+#include "minirt.h"
+
+int	deal_key(int key, void *param)
+{
+	ft_putchar('X');
+}
 
 int	main (void)
 {
@@ -21,6 +25,8 @@ int	main (void)
 	printf("miniRT by fde-capu\n");//
 	x = mlx_init();
 	w = mlx_new_window(x, 500, 500, "miniRT by fde-capu");
+	mlx_pixel_put(x, w, 250, 250, 0x0000FF);
+	mlx_key_hook(w, deal_key, (void *)0);
 	mlx_loop(x);
 	return (0);
 }
