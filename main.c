130c85c2cbc5c4b2348aef546c875cbba0705e19
Flavio Carrara De Capua <fde-capu@d3614n.d3614nhost>
2020-06-09 15:25:48 -0300

    stable yet working on rgb debug

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/13 05:11:09 by fde-capu          #+#    #+#             */
-/*   Updated: 2020/06/09 12:07:28 by fde-capu         ###   ########.fr       */
+/*   Updated: 2020/06/09 15:21:37 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,8 @@ int	main(int argc, char **argv)
 	}
 	DEBINT(MSG_RX, g_scn.r.x);
 	DEBINT(MSG_RY, g_scn.r.y);
+	DEBDBL("amb force", g_scn.a.f);
+	DEBRGB("amb rgb", g_scn.a.rgb);
 	x = mlx_init();
 	w = mlx_new_window(x, g_scn.r.x, g_scn.r.y, WIN_TITLE);
 	mlx_pixel_put(x, w, 250, 250, 0xFFFFFF);
