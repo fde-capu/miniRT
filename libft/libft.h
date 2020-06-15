0515fa02ef171b4529beae4695d26f021b6636a2
Flavio Carrara De Capua <fde-capu@d3614n.d3614nhost>
2020-06-15 08:15:34 -0300

    libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 16:19:33 by fde-capu          #+#    #+#             */
-/*   Updated: 2020/06/11 16:07:22 by fde-capu         ###   ########.fr       */
+/*   Updated: 2020/06/15 08:14:32 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,7 @@ int				ft_isalnum(int c);
 int				ft_isalpha(int c);
 int				ft_isascii(int c);
 int				ft_isdigit(int c);
+int				ft_isnumber(char *str);
 int				ft_isprint(int c);
 char			*ft_itoa(int n);
 t_rgb			ft_itorgb(int i);
@@ -143,6 +144,7 @@ u_int8_t		ft_bit8_get(u_int8_t *data, u_int8_t bit);
 void			ft_bit8_set(u_int8_t *data, u_int8_t bit, u_int8_t val);
 void			ft_bit8_tog(u_int8_t *data, u_int8_t bit);
 char			*ft_bit8_str(u_int8_t data);
+char			*ft_removequotes(char *quoted);
 
 # define TRIM_SET			" \t\n\r\v\f"
 # define COMMENT_SET		"#"
