ebb9768b495cc3b69ef2a6443e0836fa02d44efc
Flavio Carrara De Capua <flavio@carrara.us>
2020-06-29 14:17:00 -0300

    fishing forgotten files

---
+++ libft/ft_rgb2.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rgb2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/29 09:41:33 by fde-capu          #+#    #+#             */
+/*   Updated: 2020/06/29 09:42:34 by fde-capu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int				ft_rgbtoi(t_rgb rgb)
+{
+	int	o;
+
+	o = 0;
+	o += (rgb.r << 16);
+	o += (rgb.g << 8);
+	o += (rgb.b);
+	return (o);
+}
+
+unsigned int	ft_argbtoi(t_rgb rgb)
+{
+	unsigned int	o;
+
+	o = 0;
+	o += (rgb.a << 24);
+	o += (rgb.r << 16);
+	o += (rgb.g << 8);
+	o += (rgb.b);
+	return (o);
+}
+
+t_rgb			ft_itobgra(unsigned int i)
+{
+	t_rgb	rgb;
+
+	rgb.b = (i & 0xFF000000) >> 24;
+	rgb.g = (i & 0x00FF0000) >> 16;
+	rgb.r = (i & 0X0000FF00) >> 8;
+	rgb.a = (i & 0x000000FF) >> 0;
+	return (rgb);
+}
+
+t_rgb			ft_itoargb(unsigned int i)
+{
+	t_rgb	rgb;
+
+	rgb.a = (i & 0xFF000000) >> 24;
+	rgb.r = (i & 0x00FF0000) >> 16;
+	rgb.g = (i & 0X0000FF00) >> 8;
+	rgb.b = (i & 0x000000FF) >> 0;
+	return (rgb);
+}
---
+++ render_1.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_1.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/29 13:32:27 by fde-capu          #+#    #+#             */
+/*   Updated: 2020/06/29 13:42:49 by fde-capu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+void	render(t_mrt *mrt)
+{
+	ft_putstr(MSG_RENDERING);
+	ft_col(mrt, 0x99FF0000);
+	ft_mov(mrt, 1, 1);
+	ft_pxi(mrt);
+	ft_col(mrt, 0x0000FF00);
+	ft_mov(mrt, 2, 2);
+	ft_pxi(mrt);
+	ft_col(mrt, 0x000000FF);
+	ft_mov(mrt, 3, 3);
+	ft_pxi(mrt);
+	return ;
+}
+
+void	flip(t_mrt *mrt)
+{
+	mlx_put_image_to_window(mrt->mlx, mrt->win, mrt->img, 0, 0);
+	return ;
+}
